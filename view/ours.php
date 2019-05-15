0f14ccae41685f84ff0edd6b1c20c5efdeea1887
nusr <nusr@protonmail.com>
2019-05-15 11:35:17 +0200

    Ajout de la page ours.php

---
+++ view/ours.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html lang="fr">
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <title>Billet simple pour l'Alaska : photo exclusive</title>
+
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+        <link rel="stylesheet" href="public/styles/styles.css">
+
+    </head>
+
+    <body>
+
+        <div><a href="index.php"><img id="bear_pic" alt="Ours polaire" src="public/assets/ours.jpg"></a></div>
+
+    </body>
+
+</html>
